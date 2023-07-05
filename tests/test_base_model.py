df80d94213bce981243a165788f56aa35bf99060
solp22 <sol.puentereal@gmail.com>
2023-07-05 12:37:31 -0700

    add test

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -1,15 +1,14 @@
-#!/usr/bin/pyhton3
-
-import unittest
+#!/usr/bin/python3
 from models.base_model import BaseModel
 
-
-class TestBaseModel(unittest.TestCase):
-    """"""
-    pass
-
-    def test_id(self):
-        """"""
-        a1 = BaseModel() 
-        
-        self.assertEqual(a1.id)
+my_model = BaseModel()
+my_model.name = "My First Model"
+my_model.my_number = 89
+print(my_model)
+my_model.save()
+print(my_model)
+my_model_json = my_model.to_dict()
+print(my_model_json)
+print("JSON of my_model:")
+for key in my_model_json.keys():
+    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
