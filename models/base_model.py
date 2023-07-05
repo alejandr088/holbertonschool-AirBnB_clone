b5f31454f42b7bfc20604b0682f491a8348f7559
solp22 <sol.puentereal@gmail.com>
2023-07-05 11:48:01 -0700

    add documentation

--- models/base_model.py
+++ models/base_model.py
@@ -1,20 +1,26 @@
 #!/usr/bin/pyhton3
 import uuid
 from datetime import datetime
+"""BaseModel module"""
 
 class BaseModel:
+    """define BaseModel class"""
     def __init__(self):
+        """initialize class"""
         self.id = uuid.uuid4()
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
     def __str__(self):
+        """print string"""
         return (f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
 
     def save(self):
+        """updates updated_at with the current datetime"""
         self.updated_at = datetime.now()
 
     def to_dict(self):
+        """returns a dictionary containing all keys/values"""
         instance_dict = self.__dict__
         instance_dict["__class__"] = self.__class__.__name__
         instance_dict["created_at"] = self.created_at.isoformat()
