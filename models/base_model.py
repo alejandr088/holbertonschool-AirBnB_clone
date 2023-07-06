72d5b9c81da4e61517cc5dfd96a443bab0c7f8bf
solp22 <sol.puentereal@gmail.com>
2023-07-06 06:24:25 -0700

    recreate instance with the dictionary representation

--- models/base_model.py
+++ models/base_model.py
@@ -12,11 +12,11 @@ class BaseModel:
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
         if kwargs is not None or len(kwargs) != 0:
-            kwargs = self.__name__
-            
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key == "created_at" or key == "updated_at":
+                        value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                    setattr(self, key, value)
 
     def __str__(self):
         """print string"""
