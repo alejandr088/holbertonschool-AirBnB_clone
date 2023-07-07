bcdc47720511337e2c5efe01571114df606c4a93
solp22 <sol.puentereal@gmail.com>
2023-07-07 05:14:42 -0700

    change self to FileStorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -11,19 +11,19 @@ class FileStorage:
     
     def all(self):
         """return the dictionary __objects"""
-        return self.__objects
+        return FileStorage.__objects
 
     def new(self, obj):
         """sets items in the __objects dictionary"""
         key = obj.__class__.__name__ + "." + obj.id
-        self.__objects[key] = obj
+        FileStorage.__objects[key] = obj
 
     def save(self):
         """serialize to JSON file"""
         new_dict = {}
-        for key, value in self.__objects.items():
+        for key, value in FileStorage.__objects.items():
             new_dict[key] = value.to_dict()
-        with open(self.__file_path, 'w', encoding="utf-8") as f:
+        with open(FileStorage.__file_path, 'w', encoding="utf-8") as f:
             json.dump(new_dict, f)
 
     def reload(self):
@@ -34,6 +34,6 @@ class FileStorage:
         if os.path.exists(self.__file_path):
             with open(self.__file_path, 'r', encoding="utf-8") as f:
                 content_dict = json.load(f)
-                self.__objects = {}
+                FileStorage.__objects = {}
                 for key, value in content_dict.items():
-                    self.__objects[key] = BaseModel(**value)
+                    FileStorage.__objects[key] = BaseModel(**value)
