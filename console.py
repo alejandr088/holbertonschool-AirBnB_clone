d9cae62a2864a837e95f6e3234f3a66d539f9b68
solp22 <sol.puentereal@gmail.com>
2023-07-07 11:01:47 -0700

    add destroy, show and create to console

--- console.py
+++ console.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 """HBNBCommand module"""
 import cmd
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
     """Define class"""
     prompt = "(hbnb) "
+    classes = {"BaseModel"}
     
     def do_EOF(self, arg):
         """Exit the program with EOF"""
@@ -16,8 +20,52 @@ class HBNBCommand(cmd.Cmd):
         return True
 
     def emptyline(self):
-        """an empty line doesn't execute anything"""
+        """An empty line doesn't execute anything"""
         pass
+    
+    def do_create(self, arg):
+        """Creates a new instance and saves it"""
+        if not arg:
+            print("** class name missing **")
+        elif arg not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+        else:
+            instance = eval(arg)()
+            print(instance.id)
+            instance.save()
+    
+    def do_show(self, arg):
+        """string rep. of an instance based on the class name"""
+        inpt = arg.split()
+        if not arg:
+            print("** class name missing **")
+        elif inpt[0] not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+        elif len(inpt) == 1:
+            print("** instance id missing **")
+        else:
+            key = inpt[0] + "." + inpt[1]
+            if key not in storage.all():
+                print("** no instance found **")
+            else:
+                print(storage.all()[key])
+
+    def do_destroy(self, arg):
+        """Deletes an instance"""
+        inpt = arg.split()
+        if not arg:
+            print("** class name missing **")
+        elif inpt[0] not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+        elif len(inpt) == 1:
+            print("** instance id missing **")
+        else:
+            key = inpt[0] + "." + inpt[1]
+            if key not in storage.all():
+                print("** no instance found **")
+            else:
+                del(storage.all()[key])
+                storage.save()
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
