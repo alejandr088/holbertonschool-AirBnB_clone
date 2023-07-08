14c94b3b9cd402e1a3181cd11ca7fa1e5d69f32c
solp22 <sol.puentereal@gmail.com>
2023-07-08 13:43:47 -0700

    implement other classes to console, clean garbage files

--- console.py
+++ console.py
@@ -3,13 +3,20 @@
 import cmd
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
     """Define class"""
     prompt = "(hbnb) "
-    classes = ["BaseModel"]
+    classes = ["BaseModel", "User", "State", "City"
+               "Amenity", "Place", "Review"]
 
     def do_EOF(self, arg):
         """Exit the program with EOF"""
